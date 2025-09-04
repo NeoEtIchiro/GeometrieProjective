5afddfe766bcc472ac6b29c55f25c9f2936d9b3f
Neo <neoetichiro@gmail.com>
2025-09-04 11:05:18 +0200

    correction

--- code_rotation_basique.py
+++ code_rotation_basique.py
@@ -11,51 +11,45 @@ la séquence d'images est ensuite exportée dans une animation
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-
 from math import pi, cos, sin
-import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from matplotlib import rc
 
+# Triangle initial
+xtriangle = np.array([1, 1.5, 1.25])
+ytriangle = np.array([1, 1, 1.5])
 
-## affichage d'un triangle
-xtriangle = np.array([1,1.5,1.25])
-ytriangle = np.array([1,1,1.5])
-
-plt.figure(1)
-plt.fill(xtriangle, ytriangle, facecolor='none', edgecolor='red')
-plt.plot(xtriangle,ytriangle, '*')
-plt.axis([-2,2,0,4])
-plt.show()
-
-
-# Rotation 
-def RotationOrigine(t,x,y):
-    # rotation d'angle t (en radians) autour de l'origine
-    # appliquées aux points dont la liste des abscisses est x, et liste des ordonnées y
-    M = np.array([[cos(t), -sin(t)],[sin(t),cos(t)]])
-    v = np.array([x,y])  # on regroupe l'ensemble des points dans une matrice
+def RotationOrigine(t, x, y):
+    M = np.array([[cos(t), -sin(t)], [sin(t), cos(t)]])
+    v = np.array([x, y])
     TriangleTourne = M.dot(v)
     return TriangleTourne
 
-#%%# on crée les images de la sequence en les mettant dans un objet "list"
-N=90  # nombre d'images dans la sequence
-fig = plt.figure(2)
-plt.axis('equal') # pour ne pas avoir l'impression que le triangle se déforme
+# Animation parameters
+N = 90
+angle_total = pi / 2
 
-angle_total = pi/2
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+ax.set_xlim(-2, 2)
+ax.set_ylim(0, 4)
 
-liste_images = []
-for j in range(N+1):
-    v = RotationOrigine(angle_total*j/N,xtriangle,ytriangle)
-    [xi,yi] = v
-    im = plt.fill(xi, yi, facecolor='none', edgecolor='red')+plt.plot(xi,yi, 'bo')+plt.plot(0,0,'go')
-    liste_images.append(im)
+# Initial plot
+triangle_patch = ax.fill(xtriangle, ytriangle, facecolor='none', edgecolor='red')[0]
+points_plot, = ax.plot(xtriangle, ytriangle, 'bo')
+origin_plot, = ax.plot(0, 0, 'go')
 
+def animate(j):
+    v = RotationOrigine(angle_total * j / N, xtriangle, ytriangle)
+    xi, yi = v
+    triangle_patch.set_xy(np.column_stack([xi, yi]))
+    points_plot.set_data(xi, yi)
+    # origin_plot stays the same
+    return triangle_patch, points_plot, origin_plot
 
-## export dans une animation gif
-anim = animation.ArtistAnimation(fig, liste_images)#, interval=150, repeat_delay=1000)
-anim.save("RotationTriangle1.gif")
-rc('animation', html='jshtml')
-anim
+ani = animation.FuncAnimation(fig, animate, frames=N+1, interval=50, blit=True)
+
+# Pour afficher l'animation dans la fenêtre interactive
+plt.show()
+
+# Pour sauvegarder en GIF
+ani.save("RotationTriangle1.gif", writer='pillow')
