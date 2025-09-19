34a4ea0eea0c609cb8895db7e5044faee2354d18
Paquies <paquiesther1@gmail.com>
2025-09-19 16:36:25 +0200

    Fix author name in docstring

--- Questions/code_qu5.py
+++ Questions/code_qu5.py
@@ -6,7 +6,7 @@ Created on Wed Aug 27 13:53:56 2025
 ce code crée un triangle et le fait tourner autour de l'origine
 la séquence d'images est ensuite exportée dans une animation
 
-@author: fehren
+@author: fehre
 """
 
 import math
